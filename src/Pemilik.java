267daa2f1ece0ba1439339cf808376555b3fc6a2
REDummy <215314181@student.usd.ac.id>
2023-05-04 12:56:12 +0700

    Created Pemilik class and subsequent methods

---
+++ src/Pemilik.java
@@ -0,0 +1,38 @@
+public class Pemilik {
+    private String nikPemilik;
+    private String namaPemilik;
+    private String uidPemilik;
+    private String passPemilik;
+
+    public String getNikPemilik() {
+        return nikPemilik;
+    }
+
+    public void setNikPemilik(String nikPemilik) {
+        this.nikPemilik = nikPemilik;
+    }
+
+    public String getNamaPemilik() {
+        return namaPemilik;
+    }
+
+    public void setNamaPemilik(String namaPemilik) {
+        this.namaPemilik = namaPemilik;
+    }
+
+    public String getUidPemilik() {
+        return uidPemilik;
+    }
+
+    public void setUidPemilik(String uidPemilik) {
+        this.uidPemilik = uidPemilik;
+    }
+
+    public String getPassPemilik() {
+        return passPemilik;
+    }
+
+    public void setPassPemilik(String passPemilik) {
+        this.passPemilik = passPemilik;
+    }
+}
