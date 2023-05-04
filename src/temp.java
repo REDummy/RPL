8a332868f9536ef738cf4fb3754e027eb3dc6e87
REDummy <215314181@student.usd.ac.id>
2023-05-04 13:28:21 +0700

    Updated

---
+++ src/temp.java
@@ -0,0 +1,16 @@
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.Date;
+
+public class temp {
+    public static LocalDateTime temp = LocalDateTime.now();
+
+    public static LocalTime temp2 = LocalTime.now();
+
+
+    public static void main(String[] args) {
+        System.out.println(temp);
+        System.out.println(temp2);
+    }
+}
